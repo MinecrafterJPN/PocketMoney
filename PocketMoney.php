118951df5917b8baa86aca59c941c30d18613b73
MinecrafterJPN <tearjerker.k@gmail.com>
2013-07-30 10:25:34 +0900

    Improved and Implemented money.changed

--- PocketMoney.php
+++ PocketMoney.php
@@ -12,8 +12,8 @@ apiversion=9
 
 class PocketMoney implements Plugin
 {
-	private $api, $path;
-	
+	private $api, $config, $path;
+
 	const DEFAULT_MONEY = 500;
 
 	public function __construct(ServerAPI $api, $server = false)
@@ -23,256 +23,143 @@ class PocketMoney implements Plugin
 
 	public function init()
 	{
+		$this->config = new Config($this->api->plugin->configPath($this) . "config.yml", CONFIG_YAML);
 		$this->api->addHandler("player.join", array($this, "eventHandler"));
 		$this->api->addHandler("money.handle", array($this, "eventHandler"));
 		$this->api->addHandler("money.player.get", array($this, "eventHandler"));
 		$this->api->console->register("money", "PocketMoney command", array($this, "commandHandler"));
-
 		$this->path = $this->api->plugin->createConfig($this, array());
 	}
 
 	public function eventHandler($data, $event)
 	{
-		$cfg = $this->api->plugin->readYAML($this->path . "config.yml");
-		switch($event)
-		{
+		switch ($event) {
 			case "player.join":
 				$target = $data->username;
-				if(!array_key_exists($target, $cfg))
-				{
-					$this->api->plugin->createConfig($this,array(
-							$target => array(
-									'money' => self::DEFAULT_MONEY
-							)
-					));
-					$this->api->chat->broadcast("[PocketMoney]$target has been registered.");
+				if (!$this->config->exists($target)) {
+					$this->config->set($target, array('money' => self::DEFAULT_MONEY));
+					$this->api->chat->broadcast("[PocketMoney] $target has been registered");
 				}
 				break;
 			case "money.handle":
-				if(!isset($data['username']) or !isset($data['method']) or !isset($data['amount'])) return false;
+				if(!isset($data['username']) or !isset($data['method']) or !isset($data['amount']) or !is_numeric($data['amount'])) return false;
+				$issuer = isset($data['issuer']) ? $data['issuer'] : "undefined";
 				$target = $data['username'];
 				$method = $data['method'];
-				$amount = (int)$data['amount'];
-				if($this->api->player->get($target) === false or !array_key_exists($target, $cfg) or !is_numeric($amount))
-				{
+				$amount = $data['amount'];
+				if ($this->api->player->get($target) === false or !$this->config->exists($target)) {
 					return false;
 				}
-				switch($method)
-				{
+				switch ($method) {
 					case "set":
-						if($amount < 0)
-						{
+						if ($amount < 0) {
 							return false;
 						}
-						$result = array(
-								$target => array(
-										'money' => $amount
-								)
-						);
-						$this->overwriteConfig($result);
+						$this->config->set($target, array('money' => $amount));
 						break;
 					case "grant":
-						$targetMoney = $cfg[$target]['money'] + $amount;
+						$targetMoney = $this->config->get($target)['money'] + $amount;
 						if($targetMoney < 0) return false;
-						$result = array(
-								$target => array(
-										'money' => $targetMoney
-								)
-						);
-						$this->overwriteConfig($result);
+						$this->config->set($target, array('money' => $targetMoney));
 						break;
 					default:
 						return false;
 				}
+				$this->api->dhandle("money.changed", array(
+						'issuer' => $issuer,
+						'target' => $target,
+						'method' => $method,
+						'amount' => $amount
+				)
+				);
 				return true;
 			case "money.player.get":
-				if(array_key_exists($data['username'], $cfg))
-				{
-					return $cfg[$data['username']]['money'];
+				if ($this->config->exists($data['username'])) {
+					return $this->config->get($data['username'])['money'];
 				}
 				return false;
 		}
-	}
-	public function userCommandHandler($cmd, $args, $issuer, $alias)
-	{
-		$output = "";
-		switch($cmd){
-			case "money":
-				$subCommand = $args[0];
-				$cfg = $this->api->plugin->readYAML($this->path . "config.yml");
-				switch($subCommand){
-					case "":
-						if(!array_key_exists($issuer->username, $cfg))
-						{
-							$output .= "[PocketMoney]You have not been registered.";
-							break;
-						}
-						$money = $cfg[$issuer->username]['money'];
-						$output .= "[PocketMoney]$money PM";
-						break;
-					case "pay":
-						$target = $args[1];
-						$payer = $issuer->username;
-						if($target === $payer)
-						{
-							$output .= "[PocketMoney]You cannot pay yourself.";
-							break;
-						}
-						if(!$this->api->player->get($target))
-						{
-							$output .= "[PocketMoney]$target is not in the server now.";
-							break;
-						}
-						$targetMoney = $cfg[$target]['money'];
-						$payerMoney = $cfg[$payer]['money'];
-						$amount = $args[2];
-						if(!is_numeric($amount) or $amount < 0 or $amount > $payerMoney)
-						{
-							$output .= "[PocketMoney]Invalid amount.";
-							break;
-						}
-						$targetMoney += $amount;
-						$payerMoney -= $amount;
-						$result = array(
-								$payer => array(
-										'money' => $payerMoney
-								),
-								$target => array(
-										'money' => $targetMoney
-								)
-						);
-						$this->overwriteConfig($result);
-						$output .= "[PocketMoney]Completed pay process.";
-						$this->api->chat->sendTo(false, "[PocketMoney]$amount PM paid from $payer", $target);
-						break;
-					case "top":
-						$amount = $args[1];
-						$temp = array();
-						foreach($cfg as $name => $elements)
-						{
-							$temp[$name] = $elements['money'];
-						}
-						arsort($temp);
-						$i = 1;
-						$output .= "[PocketMoney]List of Millionaires\n";
-						foreach($temp as $name => $money)
-						{
-							if($i > $amount){
-								break;
-							}
-							$output .= "#$i : $name $money PM\n";
-							$i++;
-						}
-						break;
-					case "stat":
-						$total = 0;
-						$num = 0;
-						foreach($cfg as $name => $elements)
-						{
-							$total += $elements['money'];
-							$num++;
-						}
-						$avr = floor($total / $num);
-						$output .= "[PocketMoney]Circ:$total Avr:$avr Accounts:$num";
-						break;
-					default:
-						$output .= "[PocketMoney]Such sub command dose not exist.";
-						break;
-				}
-				break;
-		}
-		return $output;
 	}
 
 	public function commandHandler($cmd, $args, $issuer, $alias)
 	{
 		$cmd = strtolower($cmd);
-		if($issuer !== "console")
-		{
+		if ($issuer !== 'console') {
 			return $this->userCommandHandler($cmd, $args, $issuer, $alias);
 		}
-		switch($cmd)
-		{
+		switch ($cmd) {
 			case "money":
-				$cfg = $this->api->plugin->readYAML($this->path . "config.yml");
 				$subCommand = $args[0];
-				switch($subCommand)
-				{
+				switch ($subCommand) {
 					case "":
 					case "help":
 						console("[PocketMoney]/money help( or /money )");
 						console("[PocketMoney]/money set <target> <amount>");
 						console("[PocketMoney]/money grant <target> <amount>");
+						console("[PocketMoney]/money top <amount>");
+						console("[PocketMoney]/money stat");
 						break;
 					case "set":
 						$target = $args[1];
 						$amount = $args[2];
-						if(!$this->api->player->get($target))
-						{
-							console("[PocketMoney]$target is not in the server now.");
+						if ($this->api->player->get($target) === false) {
+							console("[PocketMoney] $target is not in the server now");
 							break;
 						}
-						/*if(!array_key_exists($target, $cfg))
-						 {
-						console("[PocketMoney]$target has not been registered.");
-						break;
-						} */
-						if($amount < 0 or !is_numeric($amount))
-						{
-							console("[PocketMoney]Invalid amount.");
+						if (!is_numeric($amount) or $amount < 0) {
+							console("[PocketMoney] Invalid amount");
 							break;
 						}
-						$result = array(
-								$target => array(
-										'money' => $amount
-								)
+						$this->config->set($target, array('money' => $amount));
+						console("[PocketMoney][set] Done!");
+						$this->api->chat->sendTo(false, "[PocketMoney][set] Your money was changed to $amount PM by admin", $target);
+						$this->api->dhandle("money.changed", array(
+								'issuer' => 'console',
+								'target' => $target,
+								'method' => 'set',
+								'amount' => $amount
+						)
 						);
-						$this->overwriteConfig($result);
-						console("[PocketMoney]Completed set process.");
-						$this->api->chat->sendTo(false, "[PocketMoney][set]Your money has been changed.\n$target:$amount PM", $target);
 						break;
 					case "grant":
 						$target = $args[1];
-						if(!$this->api->player->get($target))
-						{
-							console("[PocketMoney]$target is not in the server now.");
+						if ($this->api->player->get($target) === false) {
+							console("[PocketMoney] $target is not in the server now.");
 							break;
 						}
-						if(!array_key_exists($target, $cfg))
-						{
-							console("[PocketMoney]$target has not been registered.");
+						if(!$this->config->exists($target)) {
+							console("[PocketMoney] $target has not been registered.");
 							break;
 						}
 						$amount = $args[2];
-						$targetMoney = $cfg[$target]['money'] + $amount;
-						if(!is_numeric($amount) or $targetMoney < 0)
-						{
-							console("[PocketMoney]Invalid amount.");
+						$targetMoney = $this->config->get($target)['money'] + $amount;
+						if (!is_numeric($amount) or $targetMoney < 0) {
+							console("[PocketMoney] Invalid amount.");
 							break;
 						}
-						$result = array(
-								$target => array(
-										'money' => $targetMoney
-								)
+						$this->config->set($target, array('money' => $targetMoney));
+						console("[PocketMoney][grant] Done!");
+						$this->api->chat->sendTo(false, "[INFO][grant]Your money was changed to $targetMoney PM by admin", $target);
+						$this->api->dhandle("money.changed", array(
+								'issuer' => 'console',
+								'target' => $target,
+								'method' => 'grant',
+								'amount' => $amount
+						)
 						);
-						$this->overwriteConfig($result);
-						console("[PocketMoney]Completed grant process.");
-						$this->api->chat->sendTo(false, "[INFO][grant]Your money has been changed.\n$target:$targetMoney PM", $target);
 						break;
 					case "top":
 						$amount = $args[1];
 						$temp = array();
-						foreach($cfg as $name => $elements)
-						{
-							$temp[$name] = $elements['money'];
+						foreach ($this->config->getAll() as $name => $value) {
+							$temp[$name] = $value['money'];
 						}
 						arsort($temp);
 						$i = 1;
-						console("[PocketMoney]List of Millionaires");
-						foreach($temp as $name => $money)
-						{
-							if($i > $amount)
-							{
+						console("[PocketMoney] Millionaires");
+						console("===========================");
+						foreach ($temp as $name => $money) {
+							if ($i > $amount) {
 								break;
 							}
 							console("#$i : $name $money PM");
@@ -282,16 +169,15 @@ class PocketMoney implements Plugin
 					case "stat":
 						$total = 0;
 						$num = 0;
-						foreach($cfg as $name => $elements)
-						{
-							$total += $elements['money'];
+						foreach ($cfg as $k => $value) {
+							$total += $value['money'];
 							$num++;
 						}
 						$avr = floor($total / $num);
-						console("[PocketMoney]Circulation:$total Average:$avr Accounts:$num");
+						console("[PocketMoney] Circulation:$total Average:$avr Accounts:$num");
 						break;
 					default:
-						console("[PocketMoney]Such command dose not exist.");
+						console("[PocketMoney] /money $subCommand dose not exist.");
 						break;
 				}
 				break;
@@ -299,15 +185,97 @@ class PocketMoney implements Plugin
 
 	}
 
-	private function overwriteConfig($dat)
+	public function userCommandHandler($cmd, $args, $issuer, $alias)
 	{
-		$cfg = array();
-		$cfg = $this->api->plugin->readYAML($this->path . "config.yml");
-		$result = array_merge($cfg, $dat);
-		$this->api->plugin->writeYAML($this->path."config.yml", $result);
+		$output = "";
+		$cmd = strtolower($cmd);
+		switch ($cmd) {
+			case "money":
+				$subCommand = $args[0];
+				switch ($subCommand) {
+					case "":
+						$money = $this->config->get($issuer->username)['money'];
+						$output .= "[PocketMoney] $money PM";
+						break;
+					case "help":
+						$output .= "[PocketMoney]/money\n";
+						$output .= "[PocketMoney]/money help\n";
+						$output .= "[PocketMoney]/money pay <target> <amount>\n";
+						$output .= "[PocketMoney]/money top <amount>\n";
+						$output .= "[PocketMoney]/money stat\n";
+						break;
+					case "pay":
+						$target = $args[1];
+						$payer = $issuer->username;
+						if ($target === $payer) {
+							$output .= "[PocketMoney] Cannot pay yourself";
+							break;
+						}
+						if (!$this->api->player->get($target)) {
+							$output .= "[PocketMoney]$target is not in the server now";
+							break;
+						}
+						$targetMoney = $this->config->get($target)['money'];
+						$payerMoney = $this->config->get($payer)['money'];
+						$amount = $args[2];
+						if (!is_numeric($amount) or $amount < 0 or $amount > $payerMoney) {
+							$output .= "[PocketMoney] Invalid amount";
+							break;
+						}
+						$targetMoney += $amount;
+						$payerMoney -= $amount;
+						$this->config->set($target, $targetMoney);
+						$this->config->set($payer, $payerMoney);
+						$output .= "[PocketMoney][pay] Done!";
+						$this->api->chat->sendTo(false, "[PocketMoney] $amount PM paid from $payer", $target);
+						$this->api->dhandle("money.changed", array(
+								'issuer' => $issuer->username,
+								'target' => $target,
+								'method' => 'pay',
+								'amount' => $amount
+						)
+						);
+						break;
+					case "top":
+						$amount = $args[1];
+						$temp = array();
+						foreach ($this->config->getAll() as $name => $value) {
+							$temp[$name] = $value['money'];
+						}
+						arsort($temp);
+						$i = 1;
+						$output .= "[PocketMoney] Millionaires\n";
+						$output .= "===========================\n";
+						foreach ($temp as $name => $money) {
+							if ($i > $amount) {
+								break;
+							}
+							$output .= "#$i : $name $money PM\n";
+							$i++;
+						}
+						break;
+					case "stat":
+						$total = 0;
+						$num = 0;
+						foreach($this->config->getAll() as $k => $value)
+						{
+							$total += $value['money'];
+							$num++;
+						}
+						$avr = floor($total / $num);
+						$output .= "[PocketMoney] Circ:$total Avr:$avr Accounts:$num";
+						break;
+					default:
+						$output .= "[PocketMoney] /money $subCommand dose not exist.";
+						break;
+				}
+				break;
+		}
+		return $output;
 	}
 
 	public function __destruct()
 	{
+		$this->config->save();
 	}
 }
