9fef1efdd4c41d967fd52fdf61fda5a49272d4a7
MinecrafterJPN <minecrafter.mn@gmail.com>
2014-06-16 15:17:06 +0900

    Completed API

--- src/PocketMoney/PocketMoneyAPI.php
+++ src/PocketMoney/PocketMoneyAPI.php
@@ -2,6 +2,7 @@
 
 namespace PocketMoney;
 
+use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 
@@ -10,15 +11,14 @@ use PocketMoney\Error\SimpleError;
 
 class PocketMoneyAPI
 {
-
 	private static $api = null;
-	private $config, $system;
+	private $users, $system;
 
 	private function __construct()
 	{
         $dataFolder = Server::getInstance()->getPluginManager()->getPlugin("PocketMoney")->getDataFolder();
-		$this->config = new Config($dataFolder."config.yml");
-		$this->config = new Config($dataFolder."system.yml");
+		$this->users = new Config($dataFolder."user.yml");
+		$this->system = new Config($dataFolder."system.yml", Config::YAML, array("default_money" => 500));
 	}
 
 	/**
@@ -38,7 +38,7 @@ class PocketMoneyAPI
      */
     public function getDefaultMoney()
 	{
-		return $this->config->get("default_money");
+		return $this->system->get("default_money");
 	}
 
     /**
@@ -47,18 +47,18 @@ class PocketMoneyAPI
      */
     public function getMoney($account)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        return $this->config->get($account)['money'];
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, " \"$account\" dose not exist");
+        return $this->users->get($account)['money'];
 	}
 
     /**
      * @param string $account
-     * @return PlayerType
+     * @return PlayerType|SimpleError
      */
     public function getType($account)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        return $this->config->get($account)['type'];
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, " \"$account\" dose not exist");
+        return $this->users->get($account)['type'];
 	}
 
     /**
@@ -69,8 +69,8 @@ class PocketMoneyAPI
      */
     public function payMoney($sender, $receiver, $amount)
     {
-        if (!$this->config->exists($sender)) return new SimpleError(SimpleError::AccountNotExist, "\"$sender\" dose not exist");
-        if (!$this->config->exists($sender)) return new SimpleError(SimpleError::AccountNotExist, "\"$receiver\" dose not exist");
+        if (!$this->users->exists($sender)) return new SimpleError(SimpleError::AccountNotExist, " \"$sender\" dose not exist");
+        if (!$this->users->exists($sender)) return new SimpleError(SimpleError::AccountNotExist, " \"$receiver\" dose not exist");
         if (($res = $this->grantMoney($sender, -$amount)) !== true) return $res;
         if (($res = $this->grantMoney($receiver, +$amount)) !== true) return $res;
         return true;
@@ -79,14 +79,14 @@ class PocketMoneyAPI
     /**
      * @param string $account
      * @param int $amount
-     * @return bool
+     * @return bool|SimplerError
      */
     public function setMoney($account, $amount)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
         if (!is_numeric($amount) or $amount < 0) return new SimpleError(SimpleError::InvalidAmount, "Invalid amount");
-        $this->config->set($account, array_merge($this->config->get($account), array("money" => $amount)));
-        $this->config->save();
+        $this->users->set($account, array_merge($this->users->get($account), array("money" => $amount)));
+        $this->users->save();
         return true;
 	}
 
@@ -97,10 +97,37 @@ class PocketMoneyAPI
      */
     public function grantMoney($account, $amount)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        if (!is_numeric($amount) or ($this->getMoney($account) + $amount) < 0) return new SimpleError(SimpleError::InvalidAmount, "Invalid amount");
-        $this->config->set($account, array_merge($this->config->get($account), array("money" => $this->config->get($account)['money'] + $amount)));
-	    $this->config->save();
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $targetMoney = $this->getMoney($account);
+        if (!is_numeric($amount) or ($targetMoney + $amount) < 0) return new SimpleError(SimpleError::InvalidAmount, "Invalid amount");
+        $this->users->set($account, array_merge($this->users->get($account), array("money" => $targetMoney + $amount)));
+        $this->users->save();
+        return true;
+    }
+
+    /**
+     * @param string$account
+     * @param bool $hide
+     * @return bool|SimpleError
+     */
+    public function setAccountHide($account, $hide)
+    {
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $this->users->set($account, array_merge($this->users->get($account), array('hide' => $hide)));
+        $this->users->save();
+        return true;
+    }
+
+    /**
+     * @param bool $account
+     * @return bool|SimpleError
+     */
+    public function switchHide($account)
+    {
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $hide = $this->users->get($account)['hide'];
+        $this->users->set($account, array_merge($this->users->get($account), array('hide' => !$hide)));
+        $this->users->save();
         return true;
     }
 
@@ -110,9 +137,9 @@ class PocketMoneyAPI
      */
     public function hideAccount($account)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        $this->config->set($account, array_merge($this->config->get($account), array('hide' => true)));
-        $this->config->save();
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $this->users->set($account, array_merge($this->users->get($account), array('hide' => true)));
+        $this->users->save();
         return true;
 	}
 
@@ -122,9 +149,9 @@ class PocketMoneyAPI
      */
 	public function unhideAccount($account)
 	{
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        $this->config->set($account, array_merge($this->config->get($account), array('hide' => false)));
-        $this->config->save();
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $this->users->set($account, array_merge($this->users->get($account), array('hide' => false)));
+        $this->users->save();
         return true;
 	}
 
@@ -133,7 +160,7 @@ class PocketMoneyAPI
      */
     public function getNumberOfAccount()
     {
-        return count($this->config->getAll());
+        return count($this->users->getAll());
     }
 
     /**
@@ -142,7 +169,7 @@ class PocketMoneyAPI
     public function getTotalMoney()
     {
         $sum = 0;
-        foreach ($this->config->getAll() as $account) {
+        foreach ($this->users->getAll() as $account) {
             $sum += $account['money'];
         }
         return $sum;
@@ -150,17 +177,26 @@ class PocketMoneyAPI
 
     /**
      * @param string $account
-     * @param PlayerType $type
+     * @param int $type
      * @param bool $hide
      * @param bool|int $money
      * @return bool|SimpleError
      */
-    public function createAccount($account, PlayerType $type, $hide = false, $money = false)
+    public function createAccount($account, $type, $hide = false, $money = false)
     {
-        if ($this->config->exists($account)) return new SimpleError(SimpleError::AccountAlreadyExist, "\"$account\" already exists");
+        if ($this->users->exists($account)) return new SimpleError(SimpleError::AccountAlreadyExist, "\"$account\" already exists");
         $money = ($money === false ? $this->getDefaultMoney() : $money);
         if (!is_numeric($money) or $money < 0) return new SimpleError(SimpleError::InvalidAmount, "Invalid amount");
-        $this->config->set($account, array("money" => $money, "type" => $type, "hide" => $hide));
+        if (!is_numeric($type)) {
+            if (strtolower($type) === "player") {
+                $type = PlayerType::Player;
+            } elseif(strtolower($type) === "nonplayer") {
+                $type = PlayerType::NonPlayer;
+            } else {
+                return new SimpleError(SimpleError::InvalidAmount, "Invalid amount");
+            }
+        }
+        $this->users->set($account, array("money" => $money, "type" => $type, "hide" => $hide));
         return true;
     }
 
@@ -170,8 +206,8 @@ class PocketMoneyAPI
      */
     public function deleteAccount($account)
     {
-        if (!$this->config->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
-        $this->config->remove($account);
+        if (!$this->users->exists($account)) return new SimpleError(SimpleError::AccountNotExist, "\"$account\" dose not exist");
+        $this->users->remove($account);
         return true;
     }
 }
