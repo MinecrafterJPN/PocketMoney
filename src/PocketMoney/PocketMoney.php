b383d91d629f394794c811d4ebe9dd54e65953c5
MinecrafterJPN <minecrafter.mn@gmail.com>
2014-06-16 16:09:08 +0900

    Fixed main file

--- src/PocketMoney/PocketMoney.php
+++ src/PocketMoney/PocketMoney.php
@@ -2,13 +2,14 @@
 
 namespace PocketMoney;
 
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 use PocketMoney\Error\SimpleError;
-use PocketMoney\PocketMoneyAPI;
+
 use PocketMoney\constants\PlayerType;
 
 class PocketMoney extends PluginBase
@@ -43,8 +44,8 @@ class PocketMoney extends PluginBase
 						$sender->sendMessage("/money grant <target> <amount>");
 						$sender->sendMessage("/money top <amount>");
 						$sender->sendMessage("/money stat");
-						break;
-					
+                        break;
+
 					case "view":
 						$account = array_shift($args);
 						if (is_null($account)) {
@@ -161,11 +162,188 @@ class PocketMoney extends PluginBase
 						break;
 				}
 				return true;
+
+            default:
+                return false;
 		}
 	}
 
 	private function onCommandByUser(CommandSender $sender, Command $command, $label, array $args)
 	{
+        switch ($command->getName()) {
+            case "money":
+                $subCommand = strtolower(array_shift($args));
+                switch ($subCommand) {
+                    case "":
+                        $money = PocketMoneyAPI::getAPI()->getMoney($sender->getName());
+                        $sender->sendMessage("$money PM");
+                        break;
+                    case "help":
+                        $sender->sendMessage("/money help");
+                        $sender->sendMessage("/money view <account>");
+                        $sender->sendMessage("/money pay <target>");
+                        $sender->sendMessage("/money create <account>");
+                        $sender->sendMessage("/money hide <account>");
+                        $sender->sendMessage("/money unhide <account>");
+                        $sender->sendMessage("/money wd <target> <amount>");
+                        $sender->sendMessage("/money top <amount>");
+                        $sender->sendMessage("/money stat");
+                        break;
 
+                    case "view":
+                        $account = array_shift($args);
+                        if (is_null($account)) {
+                            $sender->sendMessage("Usage: /money view <account>");
+                            break;
+                        }
+
+                        $money = PocketMoneyAPI::getAPI()->getMoney($account);
+                        $type = PocketMoneyAPI::getAPI()->getType($account);
+                        if ($money instanceof SimpleError) {
+                            $sender->sendMessage($money->getDescription());
+                            break;
+                        }
+                        if ($type instanceof SimpleError) {
+                            $sender->sendMessage($type->getDescription());
+                            break;
+                        }
+                        $type =  ($type === PlayerType::Player) ? "Player" : "Non-player";
+                        $sender->sendMessage("[PocketMoney] \"$account\" money:$money PM, type:$type");
+                        break;
+
+                    case "pay":
+                        $target = array_shift($args);
+                        $amount = array_shift($args);
+                        if (is_null($target) or is_null($amount)) {
+                            $sender->sendMessage("Usage: /money pay <target> <amount>");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->payMoney($sender->getName(), $target, $amount)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        break;
+
+                    case "withdraw":
+                    case "wd":
+                        $target = array_shift($args);
+                        $amount = array_shift($args);
+                        if (is_null($target) or is_null($amount)) {
+                            $sender->sendMessage("[PocketMoney] Usage: /money wd <account> <amount>");
+                            break;
+                        }
+                        if (($type = PocketMoneyAPI::getAPI()->getType($target)) instanceof SimpleError) {
+                            $sender->sendMessage($type->getDescription());
+                            break;
+                        }
+                        if ($type !== PlayerType::NonPlayer) {
+                            $sender->sendMessage("You can withdraw money from only non-player account");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->payMoney($target, $sender->getName(), $amount)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        break;
+
+                    case "create":
+                        $account = array_shift($args);
+                        if (is_null($account)) {
+                            $sender->sendMessage("Usage: /money create <account>");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->createAccount($account)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        $sender->sendMessage(" \"{$account}\" was created");
+                        break;
+
+                    case "hide":
+                        $account = array_shift($args);
+                        if (is_null($account)) {
+                            $sender->sendMessage('Usage: /money hide <account>');
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->hideAccount($account)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        $sender->sendMessage("\"$account\" was hidden");
+                        break;
+
+                    case "unhide":
+                        $account = array_shift($args);
+                        if (is_null($account)) {
+                            $sender->sendMessage("Usage: /money unhide <account>");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->unhideAccount($account)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        $sender->sendMessage("\"{$account}\" was hidden");
+                        break;
+
+                    case "set":
+                        $target = array_shift($args);
+                        $amount = array_shift($args);
+                        if (is_null($target) or is_null($amount)) {
+                            $sender->sendMessage("Usage: /money set <target> <amount>");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->setMoney($target, $amount)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        $sender->sendMessage("[PocketMoney][set] Done!");
+                        $this->getServer()->getPlayer($target)->sendMessage("Your money was changed to $amount PM by admin");
+                        break;
+
+                    case "grant":
+                        $target = array_shift($args);
+                        $amount = array_shift($args);
+                        if (is_null($target) or is_null($amount)) {
+                            $sender->sendMessage("Usage: /money grant <target> <amount>");
+                            break;
+                        }
+                        if (($err = PocketMoneyAPI::getAPI()->grantMoney($target, $amount)) instanceof SimpleError) {
+                            $sender->sendMessage($err->getDescription());
+                            break;
+                        }
+                        $sender->sendMessage("[grant] Done!");
+                        $this->getServer()->getPlayer($target)->sendMessage("Your money was changed to $amount PM by admin");
+                        break;
+
+                    case "top":
+                        $amount = array_shift($args);
+                        if (is_null($amount)) {
+                            $sender->sendMessage("Usage: /money top <amount>");
+                            break;
+                        }
+                        $sender->sendMessage("[PocketMoney] Millionaires");
+                        $sender->sendMessage("===========================");
+                        $rank = 1;
+                        foreach (PocketMoneyAPI::getAPI()->getRanking($amount) as $name => $money) {
+                            $sender->sendMessage("#$rank : $name $money PM");
+                            $rank++;
+                        }
+                        break;
+                    case "stat":
+                        $totalMoney = PocketMoneyAPI::getAPI()->getTotalMoney();
+                        $accountNum = PocketMoneyAPI::getAPI()->getNumberOfAccount();
+                        $avr = floor($totalMoney / $accountNum);
+                        $sender->sendMessage("[PocketMoney] Circulation:$totalMoney Average:$avr Accounts:$accountNum");
+                        break;
+
+                    default:
+                        $sender->sendMessage("\"/money $subCommand\" dose not exist");
+                        break;
+                }
+                return true;
+
+            default:
+                return false;
+        }
 	}
 }
